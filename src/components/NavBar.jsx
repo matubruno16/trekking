34f5583757ae17c87187ad54a6db1af8bddf6b54
matubruno16 <matubruno16@gmail.com>
2024-03-04 18:49:17 -0300

    Correccion semantica

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import HamburgesaMenu from "/menu.png"
 export const NavBar = () => {
   return (
     <nav className="z-50">
-      <ul className="hidden md:flex items-center justify-between gap-4 text-lg text-gray-400">
+      <div className="hidden md:flex items-center justify-between gap-4 text-lg text-gray-400">
         <a
           href="/"
           className=' hover:cursor-pointer hover:border-b hover:border-b-1  border-gray-400  transition-all ease-in-out '>
@@ -21,7 +21,7 @@ export const NavBar = () => {
           className="inline-flex  animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-4 transition-colors hover:shadow-sm hover:shadow-slate-500">
           Contacto
         </a>
-      </ul>
+      </div>
       <img
         className="w-10 md:hidden"
         src={HamburgesaMenu}
